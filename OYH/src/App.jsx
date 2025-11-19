767fd1bf299901e3ae29f10ccf77435dddb98cc1
KushguptaPST <kushg0082@gmail.com>
2025-11-19 17:24:21 +0530

    add pop-Nav

--- OYH/src/App.jsx
+++ OYH/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Navbar from './components/navbar'
 import Body from './components/body'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Popnav from "./components/popnav";
+import LocationPage from "./components/LocationPage";
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Popnav />} />
+        <Route path="/location/:place" element={<LocationPage />} />
+      </Routes>
+    </BrowserRouter>
       <Body/>
     </div>
   )
