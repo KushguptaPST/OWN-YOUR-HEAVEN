f3ad250bc16c2707c7eee8272c99fda427f2d7ba
KushguptaPST <kushg0082@gmail.com>
2025-11-26 20:36:37 +0530

    Accept changes from part-1

--- OYH/src/components/body.jsx
+++ OYH/src/components/body.jsx
@@ -10,12 +10,16 @@ const Body = () => {
     setShowPop(!showPop)
   }
 
+  function Search(e){
+    console.log(e.target.value)
+  }
+
   return (
     <div>
       <div className='flex justify-center items-center h-70 bg-[url(https://img.freepik.com/free-photo/abstract-luxury-gradient-blue-background-smooth-dark-blue-with-black-vignette-studio-banner_1258-52393.jpg?semt=ais_hybrid&w=740&q=80)]'>
         
         <div className='flex relative'>
-          <input className='bg-white p-4 w-90 border-r' type="text" placeholder='Search by city,PGs,or Neighborhood' />
+          <input onInput={Search} className='bg-white p-4 w-90 border-r' type="text" placeholder='Search by city,PGs,or Neighborhood' />
           
           <p className='bg-white flex justify-center items-center w-60 border-r cursor-pointer'>
             Tue,18 Nov - Wed,19 Nov
@@ -26,7 +30,7 @@ const Body = () => {
             {guest} Guest , {room} Room
           </p>
 
-          <button className='bg-green-400 w-30 text-white text-xl cursor-pointer hover:bg-green-500'>
+          <button  className='bg-green-400 w-30 text-white text-xl cursor-pointer hover:bg-green-500'>
             Search
           </button>
 
