2a3aa33b959253d894d7bb9e984b886c0ec2f70d
KushguptaPST <kushg0082@gmail.com>
2025-11-27 15:33:40 +0530

    add featues

--- OYH/src/components/popnav.jsx
+++ OYH/src/components/popnav.jsx
@@ -14,13 +14,16 @@ const Popnav = () => {
     "Ub City",
     "IndiraNagar",
     "MarathaHalli",
-    "More Places"
+    "More Places",
+    "VijayaNagra",
+    "Kempegowda"
+
   ];
 
   return (
     <div>
       <div className="bg-white h-10 border justify-evenly flex">
-        {locations.map((loc) => (
+        {locations.slice(0,9).map((loc) => (
           <div
             key={loc}
             className="h-9 w-30 cursor-pointer flex items-center hover:text-red-500"
